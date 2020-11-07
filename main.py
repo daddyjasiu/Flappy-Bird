11424061d86f12ac25722a20d30b2563e7d90f6a
hi im happy <jasiu.skwarczek@gmail.com>
2020-11-07 18:32:59 +0100

    Create main.py
    creating main game loop, screen drawing and fixed framerate

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame, sys
+
+
+pygame.init()
+screen = pygame.display.set_mode((576, 1024))
+clock = pygame.time.Clock()
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+	pygame.display.update()
+	clock.tick(120)
